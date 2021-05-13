26631acf779cbcc9c11277e07090107b256006ca
1cbzin-95 <clodoaldo.neto.116@ufrn.edu.br>
2021-05-13 19:02:18 -0300

    criando models de Avaliaçao

--- Projeto/PontoTuristico/models.py
+++ Projeto/PontoTuristico/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from Atracoes.models import Atracao
+from Comentarios.models import Comentario
+from Avaliacoes.models import Avaliacao
 # Create your models here.
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
     atracoes = models.ManyToManyField(Atracao) #um ponto turistico pode ter muitas ataçoes ManyToMany
+    comentarios = models.ManyToManyField(Comentario)
+    Avaliacoes = models.ManyToManyField(Avaliacao)
 
     def __str__(self) :
         return self.nome
--- Projeto/pontosTuristicos/settings.py
+++ Projeto/pontosTuristicos/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'PontoTuristico',
     'Atracoes',
     'Comentarios',
+    'Avaliacoes',
 ]
 
 MIDDLEWARE = [
