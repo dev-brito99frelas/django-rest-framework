fb2dafc8bbbdccc444f24237c04f9b64dd99d849
1cbzin-95 <clodoaldo.neto.116@ufrn.edu.br>
2021-05-13 18:26:27 -0300

    add app de atracoes

--- Projeto/PontoTuristico/models.py
+++ Projeto/PontoTuristico/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from Atracoes.models import Atracao
 # Create your models here.
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
+    atracoes = models.ManyToManyField(Atracao) #um ponto turistico pode ter muitas ataçoes ManyToMany
 
     def __str__(self) :
         return self.nome
--- Projeto/pontosTuristicos/settings.py
+++ Projeto/pontosTuristicos/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'PontoTuristico',
+    'Atracoes',
+    'Comentarios',
 ]
 
 MIDDLEWARE = [
