8a7bad974db43ddff47f01123eb3c21a6e3432fb
1cbzin-95 <clodoaldo.neto.116@ufrn.edu.br>
2021-05-14 17:11:17 -0300

    add primeira rota django rest framework

---
+++ Projeto/PontoTuristico/api/serializers.py
@@ -0,0 +1,9 @@
+from django.db.models import fields
+from rest_framework.serializers import ModelSerializer
+from PontoTuristico.models import PontoTuristico
+
+class PontoTuristicoSerializer(ModelSerializer):
+    class Meta:
+        model = PontoTuristico
+        fields = {'id','nome','descricao'}
+        # fields = {'id','nome','descricao','provado','tracoes','comentarios','valiacoes','endereco'}
